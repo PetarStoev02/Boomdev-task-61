b18f7f62306e967ee235aa96962ebdfffa57fa4c
Петър Стоев <petarstoev0505@gmail.com>
2022-11-30 19:48:16 +0200

    v3

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  const hot= document.getElementsByClassName("subtitle is-6 price hot")
+
+  
 });
