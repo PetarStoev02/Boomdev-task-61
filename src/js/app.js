898b9fada23d73d8d50112bb5f235f93e0e0fbb2
Петър Стоев <petarstoev0505@gmail.com>
2022-11-30 19:52:57 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const hot= document.getElementsByClassName("subtitle is-6 price hot")
+  hot[0].innerHTML += "🔥"
+  
 
   
 });
